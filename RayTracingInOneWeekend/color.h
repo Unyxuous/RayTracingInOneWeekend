58e13af0b22ce9fe11153906304f8e517e319a26
teemo <dylanjacob46@gmail.com>
2025-01-13 23:42:03 -0800

    Just finished chapter 6!

---
+++ RayTracingInOneWeekend/color.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.h"
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	// Translate the [0,1] component values to byte range
+	int rbyte = int(255.999 * r);
+	int gbyte = int(255.999 * g);
+	int bbyte = int(255.999 * b);
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
